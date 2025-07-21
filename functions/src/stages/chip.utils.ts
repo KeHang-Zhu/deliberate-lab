1a553018c1a2da18c702fb607b8eb16585ccd2e9
Kehang Zhu <kehangzhu@gmail.com>
2025-07-21 17:08:10 -0400

    change ID to lower case and apply Michael's type setting

--- functions/src/stages/chip.utils.ts
+++ functions/src/stages/chip.utils.ts
@@ -565,8 +565,8 @@ export async function getChipOfferAssistance(
         const buy: Record<string, number> = {};
         const sell: Record<string, number> = {};
         //  changing the chip IDs to uppercase, specific to the Chip Negotiation game
-        const buyType = responseObj['suggestedBuyType']?.toUpperCase();
-        const sellType = responseObj['suggestedSellType']?.toUpperCase();
+        const buyType = responseObj['suggestedBuyType']?.toLowerCase();
+        const sellType = responseObj['suggestedSellType']?.toLowerCase();
 
         if (
           buyType &&
@@ -596,6 +596,7 @@ export async function getChipOfferAssistance(
         );
       }
       if (responseObj) {
+        console.log( responseObj);
         console.log(
           `Suggested: Give ${responseObj['suggestedSellQuantity']} ${responseObj['suggestedSellType']} to get ${responseObj['suggestedBuyQuantity']} ${responseObj['suggestedBuyType']} (${responseObj['reasoning']})`,
         );
@@ -909,6 +910,7 @@ export async function getChipResponseAssistance(
         );
       }
       if (responseObject) {
+        console.log(responseObject);
         console.log(
           `${responseObject['response']} ${responseObject['feedback']}`,
         );
